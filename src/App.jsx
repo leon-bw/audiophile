05555f4b01433edeceb9597cc3a78b45290c1697
Casper Lam <casper.lam.kh@googlemail.com>
2024-05-23 15:21:14 +0100

    add routes, router, browseRoutes and link logo to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header/header";
+import HomePage from "./pages/home/homePage";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
