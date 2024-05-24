b6b487a48b4c291a8c5abd03bd72e7363802a5de
Casper Lam <casper.lam.kh@googlemail.com>
2024-05-24 15:33:02 +0100

    add background images and responsive styling

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,14 +5,17 @@ export default function Hero() {
     <section className="hero">
       <div className="hero__divider"></div>
       <div className="hero__container">
-        <p className="hero__overline">new product</p>
-        <h1 className="hero__title">XX99 Mark II Headphones</h1>
-        <p className="hero__text">
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <button className="hero__btn">see product</button>
+        <div className="hero__content">
+          <p className="hero__overline">new product</p>
+          <h1 className="hero__title">XX99 Mark II Headphones</h1>
+          <p className="hero__text">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button className="hero__btn">see product</button>
+        </div>
       </div>
+      <div className="hero__img"></div>
     </section>
   );
 }
