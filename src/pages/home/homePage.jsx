18695f6ccf26805d8916c1635c7467ca1542a6d4
Casper Lam <casper.lam.kh@googlemail.com>
2024-06-04 12:08:44 +0100

    add speaker image and background circles

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,6 +1,7 @@
 import Hero from "../../components/hero/hero";
 import Navigation from "../../components/navigation/navigation";
 import "./homePage.scss";
+import zx9 from "../../assets/images/home/desktop/image-speaker-zx9.png";
 
 export default function HomePage() {
   return (
@@ -9,7 +10,7 @@ export default function HomePage() {
       <Navigation />
       <section className="zx9">
         <div className="zx9__container">
-          <div className="zx9__img"></div>
+          <img src={zx9} alt="ZX9 Speaker" className="zx9__img" />
           <h2 className="zx9__title">zx9 speaker</h2>
           <p className="zx9__text">
             Upgrade to premium speakers that are phenomenally built to deliver
