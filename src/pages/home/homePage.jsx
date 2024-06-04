9598356c892aebc9f32baae0ca744e32c0de98fc
Casper Lam <casper.lam.kh@googlemail.com>
2024-06-04 11:32:43 +0100

    add elements to zx9

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -7,6 +7,17 @@ export default function HomePage() {
     <>
       <Hero />
       <Navigation />
+      <section className="zx9">
+        <div className="zx9__container">
+          <div className="zx9__img"></div>
+          <h2 className="zx9__title">zx9 speaker</h2>
+          <p className="zx9__text">
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <button className="zx9__btn">see product</button>
+        </div>
+      </section>
     </>
   );
 }
