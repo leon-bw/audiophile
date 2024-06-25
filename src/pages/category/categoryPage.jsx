7b0aa39b6d0556e21bf82f01107853d6cb2573ef
Casper Lam <casper.lam.kh@googlemail.com>
2024-06-25 16:51:41 +0100

    add page for heacphones, speakers and earphones and add links to navigation

---
+++ src/pages/category/categoryPage.jsx
@@ -0,0 +1,13 @@
+import Article from "../../components/Article/Article";
+import Navigation from "../../components/navigation/navigation";
+import "./categoryPage.scss";
+
+export default function CategoryPage() {
+  return (
+    <>
+      {/* page banner */}
+      <Navigation />
+      <Article />
+    </>
+  );
+}
