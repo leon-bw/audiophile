2c06035976dbd0e89d01c9065b73b057b437e772
Casper Lam <casper.lam.kh@googlemail.com>
2024-06-27 12:16:17 +0100

    add product cards

--- src/components/productCard/productCard.jsx
+++ src/components/productCard/productCard.jsx
@@ -1,12 +1,37 @@
 import "./productCard.scss";
 
-export default function ProductCard() {
+export default function ProductCard({
+  category,
+  categoryImage,
+  newProduct,
+  name,
+  description,
+}) {
+  const { mobile, tablet, desktop } = categoryImage;
+
   return (
     <article className="product-card">
-      <div className="wrapper"></div>
-      <div className="wrapper">
-        <p className="product-card__new"></p>
-        <p className="product-card__text"></p>
+      <div className="product-card__wrapper">
+        <img
+          src={mobile}
+          alt={`${category} image`}
+          className="product-card__img--mobile"
+        />
+        <img
+          src={tablet}
+          alt={`${category} image`}
+          className="product-card__img--tablet"
+        />
+        <img
+          src={desktop}
+          alt={`${category} image`}
+          className="product-card__img--desktop"
+        />
+      </div>
+      <div className="product-card__wrapper">
+        {newProduct && <p className="product-card__new">NEW PRODUCT</p>}
+        <h5 className="product-card__name">{name}</h5>
+        <p className="product-card__description">{description}</p>
       </div>
     </article>
   );
