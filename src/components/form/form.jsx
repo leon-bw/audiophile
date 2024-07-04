4018cccdd4a720bd1c7d008bf198729878916676
Casper Lam <casper.lam.kh@googlemail.com>
2024-07-04 10:19:56 +0100

    add shipping info and payment details sections with controlled inputs

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -6,6 +6,13 @@ export default function Form() {
     userName: "",
     email: "",
     phone: "",
+    address: "",
+    postcode: "",
+    city: "",
+    country: "",
+    payMethod: "eMoney",
+    eNum: "",
+    ePin: "",
   });
 
   const changeHandler = (event) => {
@@ -50,7 +57,7 @@ export default function Form() {
             </div>
             <div className="form__wrapper">
               <label htmlFor="phone" className="form__label">
-                Phone number
+                Phone Number
               </label>
               <input
                 type="text"
@@ -66,9 +73,130 @@ export default function Form() {
         </section>
         <section className="form__section">
           <p className="form__subheader">shipping info</p>
+          <div className="form__container">
+            <div className="form__wrapper">
+              <label htmlFor="address" className="form__label">
+                Your Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                value={formData.address}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+            <div className="form__wrapper">
+              <label htmlFor="postcode" className="form__label">
+                Postcode
+              </label>
+              <input
+                type="text"
+                id="postcode"
+                name="postcode"
+                value={formData.postcode}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+            <div className="form__wrapper">
+              <label htmlFor="city" className="form__label">
+                City
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                value={formData.city}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+            <div className="form__wrapper">
+              <label htmlFor="country" className="form__label">
+                Country
+              </label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+          </div>
         </section>
         <section className="form__section">
           <p className="form__subheader">payment details</p>
+          <div className="form__container">
+            <div className="form__wrapper form__wrapper--radio">
+              <p className="form__label">Payment Method</p>
+              <div className="form__radio-option-wrapper">
+                <div className="form__radio-option">
+                  <label htmlFor="payMethod1" className="form__label">
+                    e-Money
+                  </label>
+                  <input
+                    type="radio"
+                    id="payMethod1"
+                    name="payMethod"
+                    value="eMoney"
+                    onChange={changeHandler}
+                    checked={formData.payMethod === "eMoney"}
+                    className="form__input-radio"
+                  />
+                </div>
+                <div className="form__radio-option">
+                  <label htmlFor="payMethod2" className="form__label">
+                    Cash on Delivery
+                  </label>
+                  <input
+                    type="radio"
+                    id="payMethod2"
+                    name="payMethod"
+                    value="cash"
+                    onChange={changeHandler}
+                    checked={formData.payMethod === "cash"}
+                    className="form__input-radio"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form__wrapper">
+              <label htmlFor="eNum" className="form__label">
+                e-Money Number
+              </label>
+              <input
+                type="text"
+                id="eNum"
+                name="eNum"
+                value={formData.eNum}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+            <div className="form__wrapper">
+              <label htmlFor="ePin" className="form__label">
+                e-Money PIN
+              </label>
+              <input
+                type="text"
+                id="ePin"
+                name="ePin"
+                value={formData.ePin}
+                onChange={changeHandler}
+                className="form__input"
+                placeholder="Type here..."
+              />
+            </div>
+          </div>
         </section>
       </form>
     </>
