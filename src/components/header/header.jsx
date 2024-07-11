36f7607fa6f723011504724b46b57ce81c871994
Casper Lam <casper.lam.kh@googlemail.com>
2024-07-11 14:48:33 +0100

    add mobile and tablet dropdown nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,32 @@ import burgerIcon from "../../assets/images/shared/tablet/icon-hamburger.svg";
 import logo from "../../assets/images/shared/desktop/logo.svg";
 import cartIcon from "../../assets/images/shared/desktop/icon-cart.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Navigation from "../navigation/navigation";
 
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <header className="header">
       <section className="header__container">
-        <img src={burgerIcon} alt="Burger menu" className="header__burger" />
+        <img
+          src={burgerIcon}
+          alt="Burger menu"
+          className="header__burger"
+          onClick={toggleNav}
+        />
+        {showNav && (
+          <section className="header__dropdown">
+            <div className="header__dropdown-container">
+              <Navigation setShowNav={setShowNav} />
+            </div>
+          </section>
+        )}
         <Link to={"/"}>
           <img src={logo} alt="Audiophile logo" className="header__logo" />
         </Link>
